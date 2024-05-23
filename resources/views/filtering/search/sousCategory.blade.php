ed4192297c3661cd7b184aa5bc15e6d124ab25af
khalidmk1 <103368578+khalidmk1@users.noreply.github.com>
2024-05-23 14:30:51 +0100

    work on search delete filter

--- resources/views/filtering/search/sousCategory.blade.php
+++ resources/views/filtering/search/sousCategory.blade.php
@@ -1,5 +1,4 @@
 @foreach ($soucategorys as $souscategory)
-
     <tr>
         <td>{{ $souscategory->id }}</td>
         <td>{{ $souscategory->category->category_name }}</td>
@@ -11,11 +10,13 @@
                 <img src="{{ asset('asset/update_icon.png') }}" style="height: 18px;" alt="update_icon">
             </a>
         </td>
-      {{--   <td>
-            <button type="button" data-toggle="modal" data-target="#delete_category_{{ $souscategory->id }}"
-                class="btn btn-sm btn-danger">
-                <i class="fa fa-trash" aria-hidden="true"></i>
-            </button>
-        </td> --}}
+        @if (auth()->user()->userRole->role_id == 1)
+            <td>
+                <button type="button" data-toggle="modal" data-target="#delete_Succategory_{{ $souscategory->id }}"
+                    class="btn btn-sm btn-danger">
+                    <i class="fa fa-trash" aria-hidden="true"></i>
+                </button>
+            </td>
+        @endif
     </tr>
 @endforeach
